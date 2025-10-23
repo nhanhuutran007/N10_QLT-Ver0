50606277234f423cf310dbc74147a768226ac623
HaiTDT <ngochaiip14@gmail.com>
2025-10-23 22:00:38 +0700

    giữ giao diện hợp đồng

--- QLKDPhongTro.Presentation/ViewModels/ContractManagementViewModel.cs
+++ QLKDPhongTro.Presentation/ViewModels/ContractManagementViewModel.cs
@@ -1,258 +1,97 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using QLKDPhongTro.BusinessLayer.Controllers;
+using QLKDPhongTro.BusinessLayer.DTOs;
+using QLKDPhongTro.DataLayer.Repositories;
+using QLKDPhongTro.Presentation.Utils;
 using QLKDPhongTro.Presentation.Views.Windows;
-// using QLKDPhongTro.Presentation.Commands;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
+using System.Windows;
 
 namespace QLKDPhongTro.Presentation.ViewModels
 {
-    public class RelayCommand : ICommand
+    public partial class ContractManagementViewModel : ViewModelBase
     {
-        private readonly Action _execute;
-        private readonly Func<bool> _canExecute;
+        private readonly ContractController _conTractController;
 
-        public RelayCommand(Action execute, Func<bool> canExecute = null)
-        {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            _canExecute = canExecute;
-        }
+        [ObservableProperty]
+        private ObservableCollection<ContractDto> _contracts;
 
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute?.Invoke() ?? true;
-        }
-
-        public void Execute(object parameter)
-        {
-            _execute();
-        }
-    }
-
-    public class RelayCommand<T> : ICommand
-    {
-        private readonly Action<T> _execute;
-        private readonly Func<T, bool> _canExecute;
-
-        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
-        {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-            _canExecute = canExecute;
-        }
-
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute?.Invoke((T)parameter) ?? true;
-        }
-
-        public void Execute(object parameter)
-        {
-            _execute((T)parameter);
-        }
-    }
-    public class ContractManagementViewModel : INotifyPropertyChanged
-    {
-        private string _searchText;
-        private ObservableCollection<ContractViewModel> _contracts;
-
-        public string SearchText
-        {
-            get => _searchText;
-            set
-            {
-                _searchText = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public ObservableCollection<ContractViewModel> Contracts
-        {
-            get => _contracts;
-            set
-            {
-                _contracts = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public ICommand AddNewContractCommand { get; }
-        public ICommand DeleteContractCommand { get; }
-        public ICommand EditContractCommand { get; }
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(EditContractCommand))]
+        [NotifyCanExecuteChangedFor(nameof(DeleteContractCommand))]
+        private ContractDto _selectedContract;
 
         public ContractManagementViewModel()
         {
-            Contracts = new ObservableCollection<ContractViewModel>();
-            AddNewContractCommand = new RelayCommand(AddNewContract);
-            DeleteContractCommand = new RelayCommand<ContractViewModel>(DeleteContract);
-            EditContractCommand = new RelayCommand<ContractViewModel>(EditContract);
-            
+
+            _conTractController = new ContractController(new ContractRepository());
             LoadContracts();
         }
 
-        public void LoadContracts()
+        private void LoadContracts()
         {
-            // Load sample contract data
-            Contracts.Clear();
-            
-            // Sample data
-            Contracts.Add(new ContractViewModel
+            try
             {
-                ContractName = "HD_001_2024.pdf",
-                TenantName = "Nguyễn Văn A",
-                CreatedDate = DateTime.Now.AddDays(-30),
-                EndDate = DateTime.Now.AddMonths(11),
-                ContractId = 1
-            });
-
-            Contracts.Add(new ContractViewModel
-            {
-                ContractName = "HD_002_2024.pdf",
-                TenantName = "Trần Thị B",
-                CreatedDate = DateTime.Now.AddDays(-15),
-                EndDate = DateTime.Now.AddMonths(5),
-                ContractId = 2
-            });
-
-            Contracts.Add(new ContractViewModel
-            {
-                ContractName = "HD_003_2024.pdf",
-                TenantName = "Lê Văn C",
-                CreatedDate = DateTime.Now.AddDays(-5),
-                EndDate = DateTime.Now.AddMonths(8),
-                ContractId = 3
-            });
-        }
-
-        public void FilterContracts()
-        {
-            // Implement search/filter logic here
-            if (string.IsNullOrEmpty(SearchText))
-            {
-                LoadContracts();
+                var contractList = _conTractController.GetAllHopDong();
+                Contracts = new ObservableCollection<ContractDto>(contractList);
             }
-            else
+            catch (System.Exception ex)
             {
-                // Filter contracts based on search text
-                // This is a simple implementation - you can enhance it
+                MessageBox.Show($"Lỗi tải dữ liệu: {ex.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
-        private void AddNewContract()
+        [RelayCommand]
+        private void AddContract()
         {
-            // Open add contract dialog
-            var addContractWindow = new AddContractWindow();
-            if (addContractWindow.ShowDialog() == true)
+            try
             {
-                LoadContracts(); // Refresh the list
-            }
-        }
-
-        private void DeleteContract(ContractViewModel contract)
-        {
-            if (contract != null)
-            {
-                Contracts.Remove(contract);
-                // TODO: Delete from database
-            }
-        }
-
-        private void EditContract(ContractViewModel contract)
-        {
-            if (contract != null)
-            {
-                // Open edit contract dialog
-                var editContractWindow = new EditContractWindow(contract);
-                if (editContractWindow.ShowDialog() == true)
+                var vm = new AddContractViewModel(_conTractController);
+                var win = new AddContractWindow(vm)
                 {
-                    LoadContracts(); // Refresh the list
+                    Owner = Application.Current?.Windows.OfType<Window>().FirstOrDefault(w => w.IsActive)
+                };
+
+                var result = win.ShowDialog();
+                if (result == true)
+                {
+                    LoadContracts();
                 }
             }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    public class ContractViewModel : INotifyPropertyChanged
-    {
-        private string _contractName;
-        private string _tenantName;
-        private DateTime _createdDate;
-        private DateTime _endDate;
-        private int _contractId;
-
-        public string ContractName
-        {
-            get => _contractName;
-            set
+            catch (Exception ex)
             {
-                _contractName = value;
-                OnPropertyChanged();
+                MessageBox.Show($"Lỗi khi mở form tạo hợp đồng: {ex.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
-        public string TenantName
+
+        private bool CanEditOrDelete() => SelectedContract != null;
+
+        [RelayCommand(CanExecute = nameof(CanEditOrDelete))]
+        private void EditContract()
         {
-            get => _tenantName;
-            set
-            {
-                _tenantName = value;
-                OnPropertyChanged();
-            }
+            MessageBox.Show($"Sửa hợp đồng cho: {SelectedContract.TenNguoiThue}");
         }
 
-        public DateTime CreatedDate
+        [RelayCommand(CanExecute = nameof(CanEditOrDelete))]
+        private void DeleteContract()
         {
-            get => _createdDate;
-            set
+            var result = MessageBox.Show($"Bạn có chắc chắn muốn xóa hợp đồng của '{SelectedContract.TenNguoiThue}'?", "Xác nhận xóa", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
             {
-                _createdDate = value;
-                OnPropertyChanged();
+                try
+                {
+                    _conTractController.DeleteHopDong(SelectedContract.MaHopDong);
+                    MessageBox.Show("Xóa hợp đồng thành công!");
+                    LoadContracts(); // Tải lại danh sách sau khi xóa
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show($"Lỗi khi xóa: {ex.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
-        }
-
-        public DateTime EndDate
-        {
-            get => _endDate;
-            set
-            {
-                _endDate = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public int ContractId
-        {
-            get => _contractId;
-            set
-            {
-                _contractId = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
